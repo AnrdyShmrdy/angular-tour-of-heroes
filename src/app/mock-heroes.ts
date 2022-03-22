bddb05d2d1c514c506e1e503bc711791218f7202
Andrew.Ponce <Andrew.Ponce@AssistRX.com>
2022-03-22 16:16:53 -0400

    part 2 completed of Tour of Heroes

---
+++ src/app/mock-heroes.ts
@@ -0,0 +1,14 @@
+import { Hero } from './hero';
+
+export const HEROES: Hero[] = [
+  {id: 11, name: 'Dr Nice'},
+  {id: 12, name: 'Narco'},
+  {id: 13, name: 'Bombasto'},
+  {id: 14, name: 'Fartaculous'},
+  {id: 15, name: 'Swampulous'},
+  {id: 16, name: 'Squeemus'},
+  {id: 17, name: 'Manatease'},
+  {id: 18, name: 'McFuhrer'},
+  {id: 19, name: 'Mussoweenie'},
+  {id: 20, name: 'Karmakazie'}
+];
